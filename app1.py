dabda88fce6103644123da6c08ab3dd9e9f7d5ef
Suraj <82825499+Suraj-1999-13@users.noreply.github.com>
2022-05-25 16:37:36 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,9 +1,7 @@
-from turtle import onclick
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 import cv2
 import mediapipe as mp
-import av
 import numpy as np
 from collections import Counter
 from tensorflow.keras.models import load_model
